76c1a6d99bc38e2eb068b9d63ddcecdb399f219b
Arka Banerjee <arkabanerjee381@gmail.com>
2025-03-31 12:21:01 +0530

    not first commit

--- vite.config.ts
+++ vite.config.ts
@@ -9,13 +9,31 @@ export default defineConfig({
     outDir: 'dist',
     sourcemap: false,
     minify: 'terser',
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true
+      }
+    },
     rollupOptions: {
       output: {
         manualChunks: {
-          vendor: ['react', 'react-dom'],
-          ui: ['@chakra-ui/react', '@chakra-ui/icons', 'framer-motion']
-        }
+          'vendor': ['react', 'react-dom'],
+          'ui': ['@chakra-ui/react', '@chakra-ui/icons', 'framer-motion'],
+          'icons': ['react-icons']
+        },
+        chunkFileNames: 'assets/[name]-[hash].js',
+        entryFileNames: 'assets/[name]-[hash].js',
+        assetFileNames: 'assets/[name]-[hash].[ext]'
       }
-    }
+    },
+    chunkSizeWarningLimit: 1000,
+    cssCodeSplit: true,
+    assetsInlineLimit: 4096
+  },
+  server: {
+    port: 3000,
+    strictPort: true,
+    host: true
   }
 })
