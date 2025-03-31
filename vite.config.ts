c490844788eda852ddc55abd951562746673f79d
Arka Banerjee <arkabanerjee381@gmail.com>
2025-03-31 09:48:42 +0530

    initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    minify: 'terser',
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+          ui: ['@chakra-ui/react', '@chakra-ui/icons', 'framer-motion']
+        }
+      }
+    }
+  }
 })
